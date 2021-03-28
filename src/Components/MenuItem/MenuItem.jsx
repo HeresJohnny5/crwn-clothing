ec314219898558b09a44c6d72ff731a882f12657
HeresJohnny5 <ericksonjohn01@hotmail.com>
2021-03-28 09:05:58 -0400

    Update styles for MenuItem Component.

--- src/Components/MenuItem/MenuItem.jsx
+++ src/Components/MenuItem/MenuItem.jsx
@@ -5,12 +5,8 @@ import "./MenuItem.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div 
-      style={
-        { backgroundImage: `url(${imageUrl})` }
-      }
-      className={`MenuItem ${size ? size : "null"} `}
-    >
+    <div className={`MenuItem ${size ? size : ""} `}>
+      <div style={{ backgroundImage: `url(${imageUrl})` }} className="background-image"></div>
       <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">SHOP NOW</span>
